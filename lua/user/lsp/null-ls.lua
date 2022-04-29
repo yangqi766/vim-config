d280821c7e5e254e3996c698a7b6427dd841f9f1
zhenjie ma <zhenjiema@bj-dl-083.autox.sz>
2022-04-29 11:38:32 +0800

    first commit

---
+++ lua/user/lsp/null-ls.lua
@@ -0,0 +1,58 @@
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast", "--line-length=100" } }),
+		formatting.stylua,
+		diagnostics.eslint.with({
+			prefer_local = "node_modules/.bin",
+		}),
+    diagnostics.flake8,
+	},
+	on_attach = function(client)
+		vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
+		if client.resolved_capabilities.document_formatting then
+		   vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
+})
+
+local no_really = {
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = { "markdown", "text" },
+    generator = {
+        fn = function(params)
+            local diagnostics = {}
+            -- sources have access to a params object
+            -- containing info about the current file and editor state
+            for i, line in ipairs(params.content) do
+                local col, end_col = line:find("really")
+                if col and end_col then
+                    -- null-ls fills in undefined positions
+                    -- and converts source diagnostics into the required format
+                    table.insert(diagnostics, {
+                        row = i,
+                        col = col,
+                        end_col = end_col,
+                        source = "no-really",
+                        message = "Don't use 'really!'",
+                        severity = 2,
+                    })
+                end
+            end
+            return diagnostics
+        end,
+    },
+}
+
+null_ls.register(no_really)
